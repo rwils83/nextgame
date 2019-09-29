1f26ee826d8cda4a8251539cc8cfd929b3dd678f
Ryan Wilson <rwils83@gmail.com>
2019-09-29 02:57:37 -0400

    fixed server weirdness

--- POCs/poc_telnet.py
+++ POCs/poc_telnet.py
@@ -5,16 +5,20 @@ import socket, threading, _thread
 HOST = '192.168.1.96'  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 
+def on_new_client(client_socket, addr):
+    while True:
+        msg = client_socket.recv(1024)
+        print(addr, ' >>', msg)
+        msg = input("Server >>")
+        client_socket.send(str.encode(msg + "\r\n"))
+    client_socket.close()
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print('Connected by', addr)
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
-            print(str(data.decode("utf-8")))
 
+    while True:
+
+        conn, addr = s.accept()
+        print("connection from", addr)
+        _thread.start_new_thread(on_new_client, (conn, addr))
